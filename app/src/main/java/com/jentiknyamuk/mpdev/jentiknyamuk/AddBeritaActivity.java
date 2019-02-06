79227d2793709bdb25046130df41307613fbcb1e
mahendra pratama <mahendrapratama67@gmail.com>
2019-02-06 17:25:43 +0700

    add berita activity

---
+++ app/src/main/java/com/jentiknyamuk/mpdev/jentiknyamuk/AddBeritaActivity.java
@@ -0,0 +1,13 @@
+package com.jentiknyamuk.mpdev.jentiknyamuk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddBeritaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_berita);
+    }
+}
